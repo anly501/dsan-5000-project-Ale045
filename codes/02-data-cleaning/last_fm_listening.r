b959a3eb8607872e1459a9d64a7241255e9ca643
Alex <alex@AlexdeMacBook-Pro-2.local>
2023-09-27 04:37:15 -0400

    modified the last_fm user cleaning and add last_fm listening evnet cleaning

---
+++ codes/02-data-cleaning/last_fm_listening.r
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(dplyr)
+
+# use the current working directory of the active document
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Read the data
+df_event <- read_csv("../../data/00-raw-data/last.fm.data/listening_events.tsv")
+
+# Sample 100000 rows from the dataset
+set.seed(1003)
+df_event <- df_event %>%
+  sample_n(100000)
+
+# Check with the missing value
+str(df_event)
+print(colSums(is.na(df_event)))
+print(nrow(df_event))
+print(head(df_event))
+
+# Split the tab separated column
+df_event <- df_event %>%
+  separate(`user_id\ttrack_id\talbum_id\ttimestamp`, into = c("user_id", "track_id", "album_id", "timestamp"), sep = "\t")
+
+
+# Write to the csv file
+write.csv(df_event, "../../data/01-modified-data/last.fm.data/listening_events_sample.csv", row.names = FALSE)
--- codes/02-data-cleaning/last_fm_user.r
+++ codes/02-data-cleaning/last_fm_user.r
@@ -27,5 +27,23 @@ df_user$age <- as.numeric(df_user$age)
 
 print(colSums(is.na(df_user)))
 
+
+
+# Show the distribution of age
+# use a mask to remove -1 ages
+mask <- df_user$age != -1
+hist(df_user$age[mask], main = "Distribution of Age", xlab = "Age", col = "lightblue")
+
+# Replace the -1 age with median
+df_user$age[!mask] <- median(df_user$age[mask])
+
+# remove the age over 80
+df_user <- df_user %>%
+  filter(age <= 80)
+
+# bining the age into 5-years interval
+df_user$age <- cut(df_user$age, breaks = seq(0, 80, 5), labels = c("0-5", "5-10", "10-15", "15-20", "20-25", "25-30", "30-35", "35-40", "40-45", "45-50", "50-55", "55-60", "60-65", "65-70", "70-75", "75-80"))
+
+
 # Write the cleaned data to csv file
 write.csv(df_user, "../../data/01-modified-data/last.fm.data/users_cleaned.csv", row.names = FALSE)
