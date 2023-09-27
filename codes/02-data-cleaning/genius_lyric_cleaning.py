716b902225802c15cd89478481366940364654e2
Alex <alex@AlexdeMacBook-Pro-2.local>
2023-09-27 02:21:58 -0400

    add genius lyrics cleaning

---
+++ codes/02-data-cleaning/genius_lyric_cleaning.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+# read in the data
+df_lyrics = pd.read_csv('../../data/00-raw-data/genius_lyrics.csv')
+df_lyrics_global = pd.read_csv('../../data/00-raw-data/genius_lyrics_global.csv')
+df_lyrics_2022 = pd.read_csv('../../data/00-raw-data/genius_lyrics_2022.csv')
+
+
+# union all the lyrcis dataframes into one
+df_lyrics_all = pd.concat([df_lyrics, df_lyrics_global, df_lyrics_2022], ignore_index=True)
+
+print(len(df_lyrics_all))
+# drop duplicates
+df_lyrics_all.drop_duplicates(subset=['track_id'], keep='first', inplace=True)
+
+print(len(df_lyrics_all))
+
+# Check for missing values
+print(df_lyrics_all.isnull().sum())
+
+# Drop rows with missing values
+df_lyrics_all.dropna(inplace=True)
+
+# Remove indication words
+df_lyrics_all['lyrics'] = df_lyrics_all['lyrics'].str.replace(r'\[.*\]', ',', regex=True)
+# Leave only printable characters
+df_lyrics_all['lyrics'] = df_lyrics_all['lyrics'].str.replace(r'[^\x20-\x7E]+', '', regex=True)
+
+# Turn the lyrcis into a bag of words and remove the stop words
+vectorizer = CountVectorizer(stop_words='english', min_df=3, token_pattern=r'\b[^\d\W]+\b')
+X = vectorizer.fit_transform(df_lyrics_all['lyrics'])
+df_bag = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
+
+
+df_lyrics_all = pd.concat([df_lyrics_all, df_bag], axis=1)
+
+# Drop the lyrics column
+df_lyrics_all.drop(columns=['lyrics'], inplace=True)
+
+
+
+# Write the data in a csv file
+df_lyrics_all.to_csv('../../data/01-modified-data/genius_lyrics_cleaned.csv', index=False)
