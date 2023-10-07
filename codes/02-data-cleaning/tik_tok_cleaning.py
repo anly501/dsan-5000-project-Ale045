8bcf94bdc2b5dbde9d9e3f02ac9719f05d0f6a23
Alex <alex@AlexdeMacBook-Pro-2.local>
2023-10-07 11:24:22 -0400

    add tik tok data cleaning

--- codes/02-data-cleaning/tik_tok_cleaning.py
+++ codes/02-data-cleaning/tik_tok_cleaning.py
@@ -9,3 +9,13 @@ print(df_tik.isnull().sum())
 # Check for duplicates
 duplicates = df_tik[df_tik.duplicated()]
 print(duplicates)
+
+# Drop unnecessary columns
+df_tik.drop(columns=['artist_pop', 'album'], inplace=True)
+
+# Change the column names
+df_tik.rename(columns={'trakc_name': 'track', 'artist_name': 'artist', 'popularity': 'track_pop'}, inplace=True)
+
+
+# Write the data in a csv file
+df_tik.to_csv('../../data/01-modified-data/tik_tok_cleaned.csv', index=False)
