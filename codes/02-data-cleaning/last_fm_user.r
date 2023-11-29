5b1548243c6ebef69c52e2217b822da0303f1a2b
Alex <alex@AlexdeMacBook-Pro-2.local>
2023-11-29 01:05:32 -0500

    add dimensionality_reduction notebook

--- codes/02-data-cleaning/last_fm_user.r
+++ codes/02-data-cleaning/last_fm_user.r
@@ -45,7 +45,7 @@ df_user <- df_user %>%
   filter(age <= 80)
 
 # bining the age into 5-years interval
-df_user$age <- cut(df_user$age, breaks = seq(0, 80, 5), labels = c("0-5", "5-10", "10-15", "15-20", "20-25", "25-30", "30-35", "35-40", "40-45", "45-50", "50-55", "55-60", "60-65", "65-70", "70-75", "75-80"))
+df_user$age_group <- cut(df_user$age, breaks = seq(0, 80, 5), labels = c("0-5", "5-10", "10-15", "15-20", "20-25", "25-30", "30-35", "35-40", "40-45", "45-50", "50-55", "55-60", "60-65", "65-70", "70-75", "75-80"))
 
 
 # Write the cleaned data to csv file
